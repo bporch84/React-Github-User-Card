9ecc6a74676825dddcea65c7be5ecce85a44da94
Barry Porch <bporch@ymail.com>
2021-03-02 15:16:58 -0500

    Created React App

---
+++ react-github/src/App.js
@@ -0,0 +1,19 @@
+import React from "react"
+import './App.css';
+
+class App extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      something: []
+    }
+  }
+
+  render(){
+    return(
+      <div>Hello.</div>
+    )
+  }
+}
+
+export default App;
